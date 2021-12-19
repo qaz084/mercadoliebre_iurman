5de234baf8ad91f959682a80ee5f290ff59f5554
qaz084 <jonathaniurman@gmail.com>
2021-12-19 00:54:38 -0300

    agregando transiciones y puliendo forms

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { get } = require("express/lib/response");
 const app = express();
 const path = require("path");
 
@@ -14,3 +15,8 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
+
+app.get("/login",(req,res)=>{
+
+  res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
